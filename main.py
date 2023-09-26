fd389eeca1a81a253d7f800f48b34c08d57efa80
Ahmed Hussein <wolktheline@yahoo.com>
2023-09-25 20:32:23 -0600

    Code cleaning

--- main.py
+++ main.py
@@ -24,15 +24,11 @@ for (index, row) in student_data_frame.iterrows():
 
 nato = pandas.read_csv("nato_phonetic_alphabet.csv")
 
-nato_dict = {code.letter: code.code for (letter, code) in nato.iterrows()}
+nato_dict = {row.letter: row.code for (letter, row) in nato.iterrows()}
 user_input = input("Enter a word: ").upper()
-user_list = []
-users_list = [nato_dict[word] for word in user_input]
-print(users_list)
-# for letter in user_input:
-#     fix = nato_dict.get(letter)
-#     user_list.append(fix)
-# print(user_list)
+user_list = [nato_dict[letter] for letter in user_input]
+print(user_list)
+
 
 
 
